12b7fc7aef48833c986b3eb1e93c9fed760e2c86
Utaralinov <nutaralinov26@gmail.com>
2022-05-17 12:04:48 +0500

    Ревью 8 спринта

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from posts.models import Comment, Post, Group
+from posts.models import Comment, Group, Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
